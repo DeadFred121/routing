5a5a1ab6157b78d91a172ae656cc4ee1e38a804d
DeadFred <f.cliff1@hotmail.com>
2017-12-12 11:27:28 +1000

    Created components and state for login checks. Page will render different content based on whether or not a user is logged in

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Redirect
 } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    loggedIn: false
+  }
+
+  toggleLoggedIn = () => {
+    this.setState(prevState => ({
+      loggedIn: !this.state.loggedIn
+    }))
+  }
+
   render() {
     return (
       <Router>
@@ -14,19 +26,33 @@ class App extends Component {
         
           <p>Welcome to my website</p>
 
+          <hr />
+          <button className={this.state.loggedIn && 'loggedIn'} onClick={this.toggleLoggedIn}>{ this.state.loggedIn ? 'Log out' : 'Log In' }</button>
+          <p>{ this.state.loggedIn && 'You are logged in!' }</p>
+          <hr />
+
           <strong>Navigation:</strong>
           <Link to="/">Home</Link> |
           <Link to="/hello">Hello</Link> |
+          <Link to="/hello/you">Hello you!</Link> |
           <Link to="/goodbye">Goodbye</Link> |
-          <Link to="/hello/you">Hello you!</Link>
+          <Link to="/contact">Contact</Link>
 
           <hr />
 
-          <Route path="/hello/you" component={HelloYou} />
           <Route exact path="/hello" component={Hello} />
+          <Route path="/hello/:name" component={HelloYou} />
           <Route path="/goodbye" component={Goodbye} />
-          <Route path="/contact" component={Contact} />
-          
+          <Route path="/contact" component={() => {
+            if (this.state.loggedIn) {
+              //All good, proceed
+              return <Contact />
+            } else {
+              //Kick 'em out
+              return <Redirect to="/login" />
+            }
+          }} />
+          <Route path="/login" component={Login} />
           
         </div>
       </Router>
@@ -34,8 +60,8 @@ class App extends Component {
   }
 }
 
-const HelloYou = () => (
-  <p>Hello You!</p>
+const HelloYou = ({ match }) => (
+  <p>Hello {match.params.name}</p>
 )
 
 const Hello = () => (
@@ -51,6 +77,10 @@ const Contact = () => (
     <h1>Contact me!</h1>
     <p>test@testies.com</p>
   </div>
+)
+
+const Login = ({ match }) => (
+  <p>Tech debt: login form goes here!</p>
 )
 
 export default App;
